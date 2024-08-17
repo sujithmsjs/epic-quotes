f363ddeaddb666774ace2a5f531df4d1a2f06104
Sujith <sujithmsjs@gmail.com>
2024-08-17 13:56:26 +0530

    Front-End Added
    React Front-End Code Added

---
+++ front-end/src/App.jsx
@@ -0,0 +1,100 @@
+
+
+import "popper.js";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { AddQuote } from "./applications/quotes-hunt/AddQuote";
+import AddTag from "./applications/quotes-hunt/AddTag";
+import Metrics from "./applications/quotes-hunt/Metrics";
+import Proverbs from "./applications/quotes-hunt/Proverbs";
+import QuotesList from "./applications/quotes-hunt/QuotesList";
+import QuotesListV2 from "./applications/quotes-hunt/QuotesListV2";
+import QuotesListV3 from "./applications/quotes-hunt/QuotesListV3";
+import QuotesNavBar2 from './applications/quotes-hunt/QuotesNavBar2';
+import { SearchBarTest } from "./applications/quotes-hunt/SearchBarTest";
+import ShowTags from "./applications/quotes-hunt/ShowTags";
+import { ConfirmDialogProvider } from "./context/ConfirmDialogContext";
+import { store } from "./store/store";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <QuotesNavBar2 />,
+    children: [
+      {
+        index: true,
+        element: <QuotesList />,
+      },
+      {
+        path: 'home',
+        element: <QuotesListV2 />,
+      },
+      {
+        path: 'add',
+        element: <AddQuote />
+      },
+      {
+        path: 'add/:quoteId',
+        element: <AddQuote />
+      },
+      {
+        path: 'tags',
+        element: <ShowTags />
+      },
+      {
+        path: 'proverbs',
+        element: <Proverbs />
+      },
+      {
+        path: 'add-tag',
+        element: <AddTag />
+      },
+      {
+        path: 'search-test',
+        element: <SearchBarTest />
+      },
+      {
+        path: 'metrics',
+        element: <Metrics />
+      },
+      {
+        path: 'listv3',
+        element: <QuotesListV3 />
+      },
+    ]
+  }
+])
+
+
+
+export default function App() {
+
+  // const [isDark, setDarkTheme] = useState(false);
+  // document.documentElement.setAttribute('data-bs-theme', isDark ? "dark" : "light");
+  document.documentElement.setAttribute('data-bs-theme', "dark");
+
+  return (
+    <>
+    <h1>Hello</h1>
+      <Provider store={store}>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <ToastContainer />
+        <ConfirmDialogProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </ConfirmDialogProvider>
+      </Provider>
+    </>
+  );
+}
---
+++ front-end/src/MyPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const MyPage = () => {
+    return (
+        <div class="container mt-3">
+            <h1>My Page</h1>
+            <p>Lorem ipsum text.</p>
+            <div class="card">
+                <div class="card-body">A card.</div>
+            </div>
+            <p>Some buttons:</p>
+            <button type="button" class="btn btn-primary">Primary</button>
+            <button type="button" class="btn btn-secondary">Secondary</button>
+            <button type="button" class="btn btn-success">Success</button>
+        </div>
+    )
+}
